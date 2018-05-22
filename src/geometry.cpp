e019923f5002c0611df443ab684b2b7914577110
Rewind <sploing1@gmail.com>
2018-05-22 14:17:58 -0700

    Started using Bazel build system and updated Vec3
    Added Bazel BUILD and WORKSPACE files
    Added length and normalize functions to Vec3 class
    Updated Gitignore to include Bazel auto-generated files

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template<typename T>
 class Vec3
 {
@@ -5,6 +7,23 @@ public:
 	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
 	Vec3(const T &xx) : x(xx), y(xx), z(xx) {}
 	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+
+	T length()
+	{
+		return sqrt(x * x + y * y + z * z);
+	} 
+
+	Vec3<T>& normalize()
+	{
+		T len = length();
+		if (len > 0) {
+			T invLen = 1 / len;
+			x *= invLen, y *= invLen, z *= invLen;
+		}
+
+		return *this;
+	} 
+
 	T x, y, z;
 };
 
@@ -12,7 +31,7 @@ typedef Vec3<float> Vec3f;
 
 int main(int argc, char **argv)
 {
-	Vec3<float> a;
-	Vec3f b; 
+	Vec3f v;
+	std::cout << v.length() << std::endl;
 	return 0;
 }
