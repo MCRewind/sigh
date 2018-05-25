e361a5487c30f8895beb1a03f29f41c2ca61761b
Rewind <sploing1@gmail.com>
2018-05-24 19:01:35 -0700

    Finished Vec3 class
    added basic arithmatic operators
    added << operator
    added cross product
    changed normalization method

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -8,20 +8,61 @@ public:
 	Vec3(const T &xx) : x(xx), y(xx), z(xx) {}
 	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
 
-	T length()
+	Vec3 operator + (const Vec3 &v) const
+	{ 
+		return Vec3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vec3 operator - (const Vec3 &v) const
 	{
-		return sqrt(x * x + y * y + z * z);
+		return Vec3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vec3 operator * (const T &r) const
+	{
+		return Vec3(x * r, y * r, z * r);
 	} 
 
-	Vec3<T>& normalize()
+	T norm() const
 	{
-		T len = length();
-		if (len > 0) {
-			T invLen = 1 / len;
-			x *= invLen, y *= invLen, z *= invLen;
+		return x * x + y * y + z * z;
+	}
+
+	T length() const
+	{
+		return sqrt(norm());
+	} 
+
+	T dot(const Vec3<T> &v) const
+	{
+		 return x * v.x + y * v.y + z * v.z;
+	}
+
+	Vec3<T> cross(const Vec3<T> &v) const
+	{
+		return Vec3<T>
+		(
+			y * v.z - z * v.y,
+			z * v.x - x * v.z,
+			x * v.y - y * v.x
+		); 
+	}
+
+	 Vec3& normalize()
+	{
+		T n = norm();
+		if (n > 0) 
+		{
+			T factor = 1 / sqrt(n);
+			x *= factor, y *= factor, z *= factor;
 		}
 
 		return *this;
+	} 
+
+	friend std::ostream& operator << (std::ostream &s, const Vec3<T> &v)
+	{
+		return s << '(' << v.x << ' ' << v.y << ' ' << v.z << ')';
 	} 
 
 	T x, y, z;
@@ -31,7 +72,7 @@ typedef Vec3<float> Vec3f;
 
 int main(int argc, char **argv)
 {
-	Vec3f v;
+	Vec3f v(1, 2, 3);
 	std::cout << v.length() << std::endl;
 	return 0;
 }
